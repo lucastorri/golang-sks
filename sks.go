8b39653c2579359e3b6152298adb58d081694711
Lucas Torri <lucastorri@gmail.com>
2015-02-01 02:17:20 +0100

    cli args

--- sks.go
+++ sks.go
@@ -1,10 +1,36 @@
 package main
 
-import "github.com/lucastorri/sks/server"
-import "github.com/lucastorri/sks/store"
+import (
+    "errors"
+    "flag"
+    "strings"
+    "github.com/lucastorri/sks/server"
+    "github.com/lucastorri/sks/store"
+)
+
+type config struct {
+    port int
+    store store.Store
+}
 
 func main() {
-    port := 12121
-    dir := "/Users/lucastorri/tmp/data"
-    server.New(port, store.NewFileStore(dir))
+    cfg := parseArgs()
+    server.New(cfg.port, cfg.store)
 }
+
+func parseArgs() config {
+    port := flag.Int("port", 12121, "server port")
+    storeArg := flag.String("store", "mem", "storage method")
+    flag.Parse()
+
+    storeParts := strings.Split(*storeArg, ":")
+    var s store.Store
+    switch storeParts[0] {
+        case "mem": s = store.NewMemStore()
+        case "dir": s = store.NewFileStore(storeParts[1])
+        default: panic(errors.New("Invalid store param"))
+    }
+
+    return config { *port, s }
+}
+
